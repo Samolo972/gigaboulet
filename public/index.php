dd4737a3061bb1e8c5b9169c9c02d5c2efb5d84a
Iness Ben <ine.benss@gmail.com>
2024-12-26 04:26:26 +0100

    add a new favicon

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png"href="https://gigaboulet.org/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="https://gigaboulet.org/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="https://gigaboulet.org/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://gigaboulet.org/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="https://gigaboulet.org/favicon/site.webmanifest" />
+    <title>Gigaboulet</title>
+
+</head>
 <?php
 
 use App\Kernel;
